b5a1c718babb13acccb7659f64db6e17c086d485
aritradey-CS <aritra1999.dey@gmail.com>
2023-07-08 19:02:00 +0530

    make the html code efficient and js more suitable

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-const accessKey = "_r0xDqx8xsiHmy3peY6F2KLg1MjTzTqfdLzuwSMh2-Y"
+const accessKey = "_r0xDqx8xsiHmy3peY6F2KLg1MjTzTqfdLzuwSMh2-Y";
 
-const formEl = document.querySelector("form")
-const inputEl = document.getElementById("search-input")
-const searchResults = document.querySelector("search-results")
-const showMore = document.getElementById("show-more-button")
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
 
-let inputData = ""
+let inputData = "";
 let page = 1;
 
-async function searchImage(){
-    inputData = inputEl.ariaValueMax;
-    const url = `https://api.unsplash.com/search/photo?page=${page}&query=${inputData}&client_id=${accesskey}`
+async function searchImage() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url)
-    const data = await response.json()
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const results = data.results
+  const results = data.results;
 
-    if (page === 1) {
-        searchResults.innerHTML = ""
-    }
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
 
-    results.map((result) =>{
-        const imageWrapper = document.createElement('div')
-        imageWrapper.classList.add("search-result")
-        const image = document.createElement('img')
-        image.src = result.url.small
-        image.alt = result.alt_description
-        const imageLink = document.createElement('a')
-        imageLink.href = result.link.html
-        imageLink.target = "_blank"
-        imageLink.textContent = result.alt_description
-    })
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
 }
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImage();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+showMore.addEventListener("click", () => {
+  searchImage();
+});
